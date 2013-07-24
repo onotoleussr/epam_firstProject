8ab5763bbd5df1c470be468e2e48c23c10464ffa
onotoleussr <onotole.ussr@gmail.com>
2013-07-25 04:16:39 +0600

    доработка меню программы

--- src/com/epam/epam_firstProject/MenuControl.java
+++ src/com/epam/epam_firstProject/MenuControl.java
@@ -1,17 +1,46 @@
 package com.epam.epam_firstProject;
 
+import java.util.Scanner;
+import java.io.Console;
+
 public class MenuControl {
 
+	private int selectedMenuItem = 0;
 	
 	public void showMenu(){
 		
-//		System.out.println("Для отбора по классу потребелния энергии  ");
+		System.out.println("::МЕНЮ::");
 		
+		System.out.println("Для включения некоторых приборво и расчета потребляемой мощности введите: 1");
+		System.out.println("Для сортировки по классу потребления мощьности введите: 2");
+		System.out.println("Для сортировки по потребляемой мощьности введите: 3");
+		System.out.println("Для отбора по потребляемой мощьности введите: 4\n\n");
 		
+		selectedMenuItem = getIntFromKeybord("[1,2,3,4]: ");
 		
+//		ClearConsole();
+		
+		System.out.println(selectedMenuItem);
 		
 	}
 	
+	private int getIntFromKeybord(String outText){
+		
+		int num = 0;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print(outText.toString());
+		
+		num = (int) sc.nextInt();
+		
+		return num;
+	}
 	
+//	public void ClearConsole() {
+//
+//		System.console().flush();
+//
+//	}	
 	
 }
--- src/com/epam/epam_firstProject/ProgramRunner.java
+++ src/com/epam/epam_firstProject/ProgramRunner.java
@@ -1,75 +1,14 @@
 package com.epam.epam_firstProject;
 
-import java.util.Properties;
-import java.io.FileOutputStream;
-import java.io.IOException;
-
 public class ProgramRunner {
 
 	public static void main(String[] args) {
 	
 		PropertiesContol propCont = new PropertiesContol();
-		
+		MenuControl mc = new MenuControl();
+
 		propCont.setProperties();
+		mc.showMenu();
 		
-//		DataUser du = new DataUser();
-//		du.SetList();
-//		du.test();
-//		du.FillProperties();
-		
-		//System.out.println(du.toString());
-		
-//		du.test();
-		
-		
-		
-/*	Properties testProp = new Properties();
-	testProp.put("first", 1);
-	testProp.put("second", 2);
-	testProp.put("third", 3);
-*/	
-	
-	
-	
- /* 	Properties prop = new Properties();
-  	 
-	try {
-		//set the properties value
-		prop.setProperty("database", "localhost");
-		prop.setProperty("dbuser", "mkyong");
-		prop.setProperty("dbpassword", "password");
-
-		//save properties to project root folder
-		prop.store(new FileOutputStream("test.properties"), null);
-
-	} catch (IOException ex) {
-		ex.printStackTrace();
-    }
-*/
-	
-	
-	
-	
-	
-	
-	
-	
-	//testProp.save("test.properties", "111");
-	
-	/*System.out.println(testProp);
-		
-	try
-	{
-	  os = new FileOutputStream("prop.txt");
-	         
-	  prop.save(os, "My properties");
-	  os.close();
-	}
-	catch(Exception ex)
-	{
-	  System.out.println(ex.toString()); 
-	}	
-		
-*/		
 	}
 }
